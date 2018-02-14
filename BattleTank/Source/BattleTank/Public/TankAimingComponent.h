d2b620f9626e968ea151330cd1d6fe4919b61c50
ALEX-PC\Alex <aharidy@hotmail.com>
2018-02-14 13:08:44 +0100

    Section 4, 168

--- BattleTank/Source/BattleTank/Public/TankAimingComponent.h
+++ BattleTank/Source/BattleTank/Public/TankAimingComponent.h
@@ -7,6 +7,15 @@
 #include "Engine.h"
 #include "TankAimingComponent.generated.h"
 
+//Enum for aiming state
+UENUM()
+enum class EFiringState:uint8
+{	
+	Reloading,
+	Aiming,
+	Locked
+};
+
 //Forward Declaration
 class UTankBarrel; 
 class UTankTurret;
@@ -27,7 +36,11 @@ public:
 	void SetTurretReference(UTankTurret* TurretToSet);
 
 	void AimAt(FVector WorldSpaceAim, float LaunchSpeed);
-	
+
+protected:
+	UPROPERTY(BlueprintReadOnly, Category="State")
+	EFiringState FiringState = EFiringState::Reloading;
+
 private:
 	UTankBarrel* Barrel = nullptr;
 
